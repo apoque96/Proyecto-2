92121f0321c72934bf19881d0755f62b6eb0982c
Dereck Cabrera <dereckcabrerang@gmail.com>
2023-11-11 23:29:11 -0600

    Borrado codigo que no hacía nada

--- Proyecto 2/Sistema.h
+++ Proyecto 2/Sistema.h
@@ -9,14 +9,6 @@ private:
 	Lista<Inventario^> listaInventario;
 
 public:
-	void registrar(System::String^ nombre, System::String^ principio, double dosis, int stock,
-		Proveedor^ proveedor, System::String^ caducidad, double compra, double venta);
-	void actualizar(Inventario^ inventario);
-	void informe();
-	int promedio();
-	void inventarioMedicamento(Inventario^ inventario);
-	double precioMásAlto(Proveedor^ proveedor);
-	Medicamento^ buscarCriterio();
 	void añadirProveedor(Proveedor^ proveedor);
 	Proveedor^ getProveedor(int index);
 	void añadirInventario(Inventario^ inventario);
